2e5fcd8b07c300a1ad8e618eadb0271683addb09
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-09-19 12:22:59 +0200

    Добавьте файлы проекта.

---
+++ TaskManager/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManager.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
