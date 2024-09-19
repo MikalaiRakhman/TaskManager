e90428a71184f9fd41d5c9cfd61049ea80f2abc6
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-09-19 18:43:34 +0200

    connect this project with local database where accounts will be saved

--- TaskManager/Controllers/AccountController.cs
+++ TaskManager/Controllers/AccountController.cs
@@ -1,33 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Data;
 using TaskManager.Models;
 
 namespace TaskManager.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly AppDbContext _context;
         List<Account> _accounts = new List<Account>();
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;            
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        
 
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Register(Account account)
+        public async Task<IActionResult> Register(Account account)
         {
             if (ModelState.IsValid)
             {
-                _accounts.Add(account);
+                _context.Accounts.Add(account);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
             }
-            return View();
+            return View(account);
         }
     }
 }
--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,7 +1,14 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TaskManager.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("TaskManagerAccounts")));
 
 var app = builder.Build();
 
