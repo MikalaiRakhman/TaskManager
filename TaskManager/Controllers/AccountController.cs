41235bc46bdb2b57dd88aadf0b16639af13a6ae0
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-09-19 14:58:42 +0200

    create new list _accounts in the AccountController             new method Register(Account account)

--- TaskManager/Controllers/AccountController.cs
+++ TaskManager/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Models;
 
 namespace TaskManager.Controllers
 {
     public class AccountController : Controller
     {
+        List<Account> _accounts = new List<Account>();
         public IActionResult Index()
         {
             return View();
@@ -16,6 +18,15 @@ namespace TaskManager.Controllers
 
         public IActionResult Register()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                _accounts.Add(account);
+            }
             return View();
         }
     }
--- TaskManager/Models/Account.cs
+++ TaskManager/Models/Account.cs
@@ -2,14 +2,11 @@
 {
     public class Account
     {
-        public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Role { get; set; }
-        public string PhoneNumber { get; set; }
-        public DateTime RegistrationDate { get; set; }
-        public bool IsActive { get; set; }
+        public string? Role { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
