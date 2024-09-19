f9b951f10a76a714fbd38ac3fff0fa6c19618c58
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-09-19 14:30:14 +0200

    added Login and Register Views

--- TaskManager/Models/Account.cs
+++ TaskManager/Models/Account.cs
@@ -1,12 +1,12 @@
 ﻿namespace TaskManager.Models
 {
-    public class User
+    public class Account
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        public string Password { get; set; }
         public string Role { get; set; }
         public string PhoneNumber { get; set; }
         public DateTime RegistrationDate { get; set; }
